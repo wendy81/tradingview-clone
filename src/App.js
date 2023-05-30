b0c2de390798b116af02a71be20f28fa72050316
Yusuf Yigit Aydemir <yusufyigitaydemir@gmail.com>
2023-05-30 15:07:29 +0300

    getting started

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Button } from 'flowbite-react';
+
+function App() {
+  return (
+    <div>
+      tradingview clone
+
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
