d3e6e84fb7334ca225eb7f0ac350359b03834e89
Yusuf Yigit Aydemir <yusufyigitaydemir@gmail.com>
2023-06-07 12:26:57 +0300

    devam

--- src/components/CoinDetail.js
+++ src/components/CoinDetail.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const CoinDetail = () => {
-    const params = useParams()
-    const { id } = params
+  const params = useParams();
+  const { id } = params;
 
-  return (
-    <div>{id}</div>
-  )
-}
+  const [history, setHistory] = useState();
 
-export default CoinDetail
+  useEffect(() => {
+    const options = {
+      headers: {
+        "x-access-token":
+          "coinrankingffd88d7f1f3ded6efdd4294f69bc4947eaeea7a7afd17f66",
+      },
+    };
+
+    fetch(`https://api.coinranking.com/v2/coin/${id}/history`, options)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
+  return <div>{id}</div>;
+};
+
+export default CoinDetail;
