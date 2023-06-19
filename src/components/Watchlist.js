904455d4dade67d9e91de3ec2f46693da24a81e0
Yusuf Yiğit Aydemir <yusufyigitaydemir@gmail.com>
2023-06-19 22:38:56 +0300

    devam

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { auth } from "../utils/Firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { Alert, Card } from "flowbite-react";
+import { Alert, Card, Table } from "flowbite-react";
 import { HiInformationCircle } from "react-icons/hi";
 
 const Watchlist = () => {
@@ -56,6 +56,66 @@ const Watchlist = () => {
             </a>
           </div>
         </div>
+
+        <Table hoverable className="w-full max-w-screen-xl m-auto my-5">
+        <Table.Head>
+          <Table.HeadCell></Table.HeadCell>
+          <Table.HeadCell>Rank</Table.HeadCell>
+          <Table.HeadCell>Name</Table.HeadCell>
+          <Table.HeadCell className="text-right">Price</Table.HeadCell>
+          <Table.HeadCell className="text-right">24 %</Table.HeadCell>
+          <Table.HeadCell className="text-right">Market Cap</Table.HeadCell>
+        </Table.Head>
+        {/* <Table.Body className="divide-y">
+          {currentCoins?.map((coin) => (
+            <Table.Row
+              key={coin.rank}
+              className="bg-white dark:border-gray-700 dark:bg-gray-800"
+            >
+              <Table.Cell className="!p-4">
+                <Checkbox />
+              </Table.Cell>
+              <Table.Cell>{coin.rank}</Table.Cell>
+              <Table.Cell className="flex justify-start items-center whitespace-nowrap font-medium">
+                <img src={coin.iconUrl} className="w-7"></img>
+                <Link to={`/coins/${coin.uuid}`}>
+                  <span className="mx-2 text-gray-900 dark:text-white">
+                    {coin.name}
+                  </span>
+                </Link>
+
+                <span className="text-grey-100">{coin.symbol}</span>
+              </Table.Cell>
+              <Table.Cell className="text-right">
+                {Number(coin.price).toFixed(5)}
+              </Table.Cell>
+              <Table.Cell
+                className={
+                  Number(coin.change) < 0
+                    ? "text-red-500 text-right"
+                    : "text-green-500 text-right"
+                }
+              >
+                {coin.change}
+              </Table.Cell>
+              <Table.Cell className="text-right">
+                {Number(coin.marketCap).toLocaleString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body> */}
+      </Table>
+
+      {/* <div className="flex items-center justify-center">
+        <Pagination
+          currentPage={currentPage}
+          onPageChange={(page) => {
+            setCurrentPage(page);
+          }}
+          showIcons
+          totalPages={5}
+          className="inline-block my-4"
+        /> */}
       </Card>
       )}
     </div>
